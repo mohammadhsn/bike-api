53aaf46c5c981af4296cad9bfeb81ae237a3416d
Mohammad <hosseini.mohamad94@gmail.com>
2020-02-28 23:59:25 +0330

    Exception handling trait for test cases

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,8 @@ use Laravel\Lumen\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    use InteractsWithExceptionHandling;
+
     /**
      * Creates the application.
      *
