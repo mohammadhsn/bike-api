cbc440af9cb625d31b36fc9946f4f550fb74efc2
Mohammad <hosseini.mohamad94@gmail.com>
2020-03-13 22:44:09 +0330

    Remove home action

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,14 @@ use Laravel\Lumen\Routing\Router;
 
 /* @var Router $router */
 
-$router->get('/', function () use ($router) {
-    return response()->json(['version' => $router->app->version()]);
-});
-
 $router->post('officers', 'OfficerController@store');
+
 $router->delete('officers/{id}', 'OfficerController@destroy');
 
 $router->get('bikes/{id}', 'BikeController@show');
+
 $router->patch('bikes/{id}', 'BikeController@resolve');
+
 $router->get('bikes', 'BikeController@index');
+
 $router->post('bikes', 'BikeController@store');
